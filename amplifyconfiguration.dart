543767a968c7511ae6f00f2dc62d0ba6f0cfbbe3
Preethisreev <150119880+Preethisreev@users.noreply.github.com>
2023-12-19 16:13:20 +0900

    commit models

---
+++ amplifyconfiguration.dart
@@ -0,0 +1,17 @@
+const amplifyconfig = '''{
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "LivestreamApp": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://3ist4fympjarrbjau5tqjy3o6i.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+                    "region": "ap-northeast-1",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-tgdjf7ffjze2rlwh4yt5ybvndi"
+                }
+            }
+        }
+    }
+}''';
