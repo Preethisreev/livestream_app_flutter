35f8d5e4e4769346dee5a640191ad5fa3e610751
Preethisreev <150119880+Preethisreev@users.noreply.github.com>
2024-01-18 14:21:44 +0900

    commit models

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
 const Color buttonBlue = Color(0xFF007AFF);
-const Color buttonWhite = Color(0xfff2f2f7);
+const Color buttonWhite = Color(0xffffffff);
 const Color buttonGreen = Color(0xff2b8359);
-const Color buttonTextSub = Color(0xffffffff);
+const Color buttonTextSub = Color(0xedf2f2f7);
